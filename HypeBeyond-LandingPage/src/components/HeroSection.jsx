a2aa5cebe010c53b9aabaf061625110d1e745122
goekhan-codes <goekhan.s@hotmail.com>
2026-03-03 22:21:16 +0100

    Hero-Section und Image 2.0

--- HypeBeyond-LandingPage/src/components/HeroSection.jsx
+++ HypeBeyond-LandingPage/src/components/HeroSection.jsx
@@ -1,14 +1,13 @@
-const heroImage =
-  "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGVyb3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60";
+import heroImage from "../assets/Hype&Beyond-Hero-Image.png";
+
 
 const HeroSection = () => {
   return (
     <header>
       <section className="hero-section">
         <div className="hero-content">
-          <h1>Welcome to HypeBeyond</h1>
+          <h1>Welcome to Hype&Beyond</h1>
           <p>Your ultimate destination for the latest trends and styles.</p>
-          <button className="cta-button">Shop Now</button>
         </div>
         <div className="hero-image">
           <img src={heroImage} alt="Hero" />
