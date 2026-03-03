8000092782c1a5b1c6b151c8c3a3f6dde14356dc
goekhan-codes <goekhan.s@hotmail.com>
2026-03-03 14:23:33 +0100

    Doppelte Hero-Section herausgenommen

--- HypeBeyond-LandingPage/src/components/HeroSection.jsx
+++ HypeBeyond-LandingPage/src/components/HeroSection.jsx
@@ -1,18 +1,21 @@
-const heroImage = "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGVyb3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60";
+const heroImage =
+  "https://images.unsplash.com/photo-1506744038136-46273834b3fb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aGVyb3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60";
 
 const HeroSection = () => {
   return (
-    <section className="hero-section">
-      <div className="hero-content">
-        <h1>Welcome to HypeBeyond</h1>
-        <p>Your ultimate destination for the latest trends and styles.</p>
-        <button className="cta-button">Shop Now</button>
+    <header>
+      <section className="hero-section">
+        <div className="hero-content">
+          <h1>Welcome to HypeBeyond</h1>
+          <p>Your ultimate destination for the latest trends and styles.</p>
+          <button className="cta-button">Shop Now</button>
         </div>
-        <div className="hero-image">    
-        <img src={heroImage} alt="Hero" />
+        <div className="hero-image">
+          <img src={heroImage} alt="Hero" />
         </div>
-        </section>
-    );
-    }   
+      </section>
+    </header>
+  );
+};
 
 export default HeroSection;
