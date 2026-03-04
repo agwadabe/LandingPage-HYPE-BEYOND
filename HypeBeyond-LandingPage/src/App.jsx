3e2403af13578ea0cc6be55062a87143a2794e1d
goekhan-codes <goekhan.s@hotmail.com>
2026-03-04 10:41:59 +0100

    Minor changes

--- HypeBeyond-LandingPage/src/App.jsx
+++ HypeBeyond-LandingPage/src/App.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import './App.css'
 import HeroSection from './components/HeroSection'
 import Footer from './components/Footer'
@@ -5,9 +6,13 @@ import UserRating from './components/UserRating'
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+=======
+>>>>>>> 7c8489d (Minor changes)
 import "./App.css";
 import Navbar from "./components/Navbar";
 import UserInputs from "./components/UserInputs";
+import UserRating from "./components/UserRating";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,10 +21,16 @@ function App() {
       <HeroSection />
       <UserRating />
       <UserInputs />
+<<<<<<< HEAD
       <Footer />
 
 
 </>
+=======
+      <UserRating />
+      <Footer />
+    </>
+>>>>>>> 7c8489d (Minor changes)
   );
 }
 
