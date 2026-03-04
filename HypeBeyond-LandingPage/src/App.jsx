831ecdf132caafe4aad274f2b2c21c61ccbd5ecb
goekhan-codes <goekhan.s@hotmail.com>
2026-03-04 11:16:52 +0100

    favicon

--- HypeBeyond-LandingPage/src/App.jsx
+++ HypeBeyond-LandingPage/src/App.jsx
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 import './App.css'
-import HeroSection from './components/HeroSection'
-import Footer from './components/Footer'
-import UserRating from './components/UserRating'
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-=======
->>>>>>> 7c8489d (Minor changes)
-import "./App.css";
 import Navbar from "./components/Navbar";
+import HeroSection from './components/HeroSection'
+import UserRating from './components/UserRating'
 import UserInputs from "./components/UserInputs";
-import UserRating from "./components/UserRating";
-import Footer from "./components/Footer";
+import Footer from './components/Footer'
+
 
 function App() {
   return (
@@ -21,16 +14,8 @@ function App() {
       <HeroSection />
       <UserRating />
       <UserInputs />
-<<<<<<< HEAD
-      <Footer />
-
-
-</>
-=======
-      <UserRating />
       <Footer />
     </>
->>>>>>> 7c8489d (Minor changes)
   );
 }
 
