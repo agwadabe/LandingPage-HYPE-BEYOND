20f9b6fdfd831811fb850fee369c77e1f0a40515
agwadabe <gwadabe.abdullah@gmail.com>
2026-03-04 02:49:30 +0100

    Navbar und Inputs angelegt

--- HypeBeyond-LandingPage/src/App.jsx
+++ HypeBeyond-LandingPage/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import HeroSection from './components/HeroSection'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/Navbar";
+import UserInputs from "./components/UserInputs";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <HeroSection />
-    <HeroSection />
+      <Navbar />
+      <HeroSection />
+      <UserInputs />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
