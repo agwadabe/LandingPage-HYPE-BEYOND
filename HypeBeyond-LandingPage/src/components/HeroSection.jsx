873cd98b9073684ce9fd353ad4633bf7a4ca02e1
goekhan-codes <goekhan.s@hotmail.com>
2026-03-04 10:19:42 +0100

    Logo Work

--- HypeBeyond-LandingPage/src/components/HeroSection.jsx
+++ HypeBeyond-LandingPage/src/components/HeroSection.jsx
@@ -1,10 +1,12 @@
+const logoDark = new URL("./assets/Hype&Beyond Logo Light Mode.svg", import.meta.url);
+
 const HeroSection = () => {
   return (
     <header>
       <section className="hero-section">
         <div className="hero-content">
-          <h1>Welcome to Hype&Beyond</h1>
-          <p>Your ultimate destination for the latest trends and styles.</p>
+          <h1>Welcome to</h1>
+          <img src={logoDark} alt="Hype & Beyond Logo"/>
         </div>
       </section>
     </header>
