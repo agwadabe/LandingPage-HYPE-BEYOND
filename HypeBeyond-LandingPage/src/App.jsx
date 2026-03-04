e4184cab3770af4b8e7778c618a363e58257e7ee
goekhan-codes <goekhan.s@hotmail.com>
2026-03-04 12:04:01 +0100

    Dot Effect

--- HypeBeyond-LandingPage/src/App.jsx
+++ HypeBeyond-LandingPage/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from './components/HeroSection'
+import HeroEffect from './components/HeroEffect'
 import UserRating from './components/UserRating'
 import UserInputs from "./components/UserInputs";
 import Footer from './components/Footer'
@@ -9,13 +10,14 @@ import Footer from './components/Footer'
 
 function App() {
   return (
-    <>
+    <div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]">
       <Navbar />
       <HeroSection />
+      <HeroEffect />
       <UserRating />
       <UserInputs />
       <Footer />
-    </>
+    </div>
   );
 }
 
---
+++ HypeBeyond-LandingPage/src/components/HeroEffect.jsx
@@ -0,0 +1,37 @@
+import { animate } from "animejs";
+
+const WaterDropGrid = () => {
+  return (
+    <div className="relative grid h-screen place-content-center bg-slate-900 px-8">
+      <DotGrid />
+    </div>
+  );
+};
+
+const GRID_WIDTH = 25;
+const GRID_HEIGHT = 20;
+
+const DotGrid = () => {
+  const dots = [];
+  let index = 0;
+
+  for (let i = 0; i < GRID_HEIGHT; i++) {
+    for (let j = 0; j < GRID_WIDTH; j++) {
+      dots.push(
+        <div
+          className="group cursor-crosshair rounded-full p-2 transition-colors hover:bg-slate-600"
+          data-index={index}
+          key={`${i}-${j}`}
+        >
+          <div className="dot-point h-2 w-2 rounded-full bg-gradient-to-b from-slate-700 to-slate-400 opacity-50 group-hover:from-indigo-600 group-hover:to-white"
+          data-index={index}/>
+        </div>,
+      );
+      index++;
+    }
+  }
+
+  return <div style={{ gridTemplateColumns: `repeat(${GRID_WIDTH}, 1fr)` }}>{dots}</div>;
+};
+
+export default WaterDropGrid;
