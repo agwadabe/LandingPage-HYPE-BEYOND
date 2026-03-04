ed8977fcbc5b407f4f8154f9503bf879e01049f3
agwadabe <gwadabe.abdullah@gmail.com>
2026-03-04 02:49:30 +0100

    Navbar und Inputs angelegt

--- HypeBeyond-LandingPage/src/App.jsx
+++ HypeBeyond-LandingPage/src/App.jsx
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
 import './App.css'
+<<<<<<< HEAD
+=======
+import HeroSection from './components/HeroSection'
+import Footer from './components/Footer'
+import UserRating from './components/UserRating'
+=======
+>>>>>>> 20f9b6f (Navbar und Inputs angelegt)
+>>>>>>> b1fbfda (Navbar und Inputs angelegt)
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from './components/HeroSection'
