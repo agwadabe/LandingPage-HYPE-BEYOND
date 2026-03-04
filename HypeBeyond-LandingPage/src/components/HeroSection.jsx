badbf649f6570adb144b7a40efa2bd14a833ed78
goekhan-codes <goekhan.s@hotmail.com>
2026-03-04 09:50:05 +0100

    HeroSection + App.css + Index.css

--- HypeBeyond-LandingPage/src/components/HeroSection.jsx
+++ HypeBeyond-LandingPage/src/components/HeroSection.jsx
@@ -1,6 +1,3 @@
-import heroImage from "../assets/Hype&Beyond-Hero-Image.png";
-
-
 const HeroSection = () => {
   return (
     <header>
@@ -8,9 +5,6 @@ const HeroSection = () => {
         <div className="hero-content">
           <h1>Welcome to Hype&Beyond</h1>
           <p>Your ultimate destination for the latest trends and styles.</p>
-        </div>
-        <div className="hero-image">
-          <img src={heroImage} alt="Hero" />
         </div>
       </section>
     </header>
