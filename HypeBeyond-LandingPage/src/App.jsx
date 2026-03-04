7c8489dc1ab65426ae19d48d7c4c714d9ddda1d5
goekhan-codes <goekhan.s@hotmail.com>
2026-03-04 10:41:59 +0100

    Minor changes

--- HypeBeyond-LandingPage/src/App.jsx
+++ HypeBeyond-LandingPage/src/App.jsx
@@ -1,33 +1,18 @@
-<<<<<<< HEAD
-import './App.css'
-import HeroSection from './components/HeroSection'
-import Footer from './components/Footer'
-import UserRating from './components/UserRating'
-
-function App() {
-  return (
-    <>
-      <HeroSection />
-      <UserRating />
-      <Footer />
-=======
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import UserInputs from "./components/UserInputs";
+import UserRating from "./components/UserRating";
+import Footer from "./components/Footer";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
       <HeroSection />
       <UserInputs />
->>>>>>> 20f9b6f (Navbar und Inputs angelegt)
+      <UserRating />
+      <Footer />
     </>
   );
 }
