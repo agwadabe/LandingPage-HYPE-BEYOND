93159d5a92d38f29aeb6dd25c1e7bb4d1531f325
agwadabe <gwadabe.abdullah@gmail.com>
2026-03-04 10:42:53 +0100

    merge gepr

--- HypeBeyond-LandingPage/src/App.jsx
+++ HypeBeyond-LandingPage/src/App.jsx
@@ -1,34 +1,25 @@
-<<<<<<< HEAD
 import './App.css'
 import HeroSection from './components/HeroSection'
 import Footer from './components/Footer'
 import UserRating from './components/UserRating'
-
-function App() {
-  return (
-    <>
-      <HeroSection />
-      <UserRating />
-      <Footer />
-=======
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 import UserInputs from "./components/UserInputs";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <Navbar />
       <HeroSection />
+      <UserRating />
       <UserInputs />
->>>>>>> 20f9b6f (Navbar und Inputs angelegt)
-    </>
+      <Footer />
+
+
+</>
   );
 }
 
