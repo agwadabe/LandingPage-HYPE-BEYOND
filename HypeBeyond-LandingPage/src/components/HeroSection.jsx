687b01a33a761c5eca9a4f1acefaf142331a523f
goekhan-codes <goekhan.s@hotmail.com>
2026-03-05 11:57:50 +0100

    Hero Section

--- HypeBeyond-LandingPage/src/components/HeroSection.jsx
+++ HypeBeyond-LandingPage/src/components/HeroSection.jsx
@@ -3,7 +3,7 @@ import logoDark from "../assets/Hype&Beyond Logo Dark Mode.svg";
 const HeroSection = () => {
   return (
     <header>
-      <section className="hero-section">
+      <section className="hero-section z-10">
         <div className="hero-content">
           <h1>Welcome to</h1>
           <img className="hero-logo" src={logoDark} alt="Hype & Beyond Logo" />
